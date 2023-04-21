6ddffbf114a0c2704c58cf04bd1f7e679e80312b
Maria <maria.gribanova@gmail.com>
2023-04-21 23:20:57 +0100

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 // global variables
 // const itemList= ["Web-dev lectures", "Exam reading", "Complete protein intake", "Book reading"];
 // const workItem = [];
- 
+
 //database 
 mongoose.connect("mongodb+srv://mariagribanova:DCadFbcPUutm5156@cluster0.lmhz4s6.mongodb.net/todolistDB");
 // mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", { useNewUrlParser: true });
@@ -130,39 +130,41 @@ app.post("/delete", (req, res) => {
 
 // routings
 // app.get("/custom/:customListName", function(req,res){
-  app.get("/:customListName", function (req, res) {
-    const customListName = _.capitalize(req.params.customListName);
-    if (customListName === "Favicon.ico") return;
-    console.log(customListName);
-    List.findOne({ name: customListName })
-      .then(foundList => {
-        if (customListName === "About") {
-          res.render("about");
-        } else if (!foundList) {
-          //Create a new list
-          const list = new List({
-            name: customListName,
-            items: defaultItems
+app.get("/:customListName", function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+  if (customListName === "Favicon.ico") return;
+  console.log(customListName);
+  List.findOne({ name: customListName })
+    .then(foundList => {
+      if (customListName === "About") {
+        res.render("about");
+      } else if (!foundList) {
+        //Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        return list.save()
+          .then(() => {
+            res.redirect("/" + customListName);
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
           });
-          return list.save()
-            .then(() => {
-              res.redirect("/" + customListName);
-            })
-            .catch(err => {
-              console.log(err);
-              throw err;
-            });
-        } else {
-          //Show an existing list
-          res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send("Error fetching list");
-      });
-  });
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+      } else {
+        //Show an existing list
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("Error fetching list");
+    });
 });
+
+mongoose.connect("mongodb+srv://mariagribanova:DCadFbcPUutm5156@cluster0.lmhz4s6.mongodb.net/todolistDB").then(
+  app.listen(3000, function () {
+    console.log("Server started on port 3000");
+  })
+);
